bfa561f41a8f51be602cdea220177d1259e94033
andrespaida <jordanpaida.bj@hotmail.com>
2025-07-02 23:38:08 -0500

    deploy

--- src/infrastructure/redis_cart_repository.js
+++ src/infrastructure/redis_cart_repository.js
@@ -5,11 +5,29 @@ class RedisCartRepository {
 
   async save(cartItem) {
     const key = `cart:${cartItem.userId}`;
-    const item = JSON.stringify({
-      productId: cartItem.productId,
-      quantity: cartItem.quantity
-    });
-    await this.redis.lpush(key, item);
+    const items = await this.redis.lrange(key, 0, -1);
+
+    let found = false;
+
+    for (let i = 0; i < items.length; i++) {
+      const item = JSON.parse(items[i]);
+
+      if (item.productId === cartItem.productId) {
+        item.quantity += cartItem.quantity;
+
+        await this.redis.lset(key, i, JSON.stringify(item));
+        found = true;
+        break;
+      }
+    }
+
+    if (!found) {
+      const item = JSON.stringify({
+        productId: cartItem.productId,
+        quantity: cartItem.quantity
+      });
+      await this.redis.rpush(key, item);
+    }
   }
 }
 
